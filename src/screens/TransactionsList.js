07e137eab2cf9be09d2c3d1d1bfae6a06da18eed
AhmadHussainTroon <ahmad.hussain@troontechnologies.com>
2021-06-08 14:29:07 +0500

    Iframe Added

--- src/screens/TransactionsList.js
+++ src/screens/TransactionsList.js
@@ -2,12 +2,26 @@ import { useMoralisQuery } from "react-moralis";
 
 export function TransactionList()
 {
+  const { data, error, isLoading } = useMoralisQuery(
+    "EthTransactions",
+    query =>
+      query
 
-  
+      ,
+    {
+      live: true,
+    },
+  );
+  const getAll=()=>{    
+  for (let i = 0; i < data.length; i++) {
+    const object = data[i];
+    console.log(object.get('hash') + ' - ' + object.get('from_address') +' - '+ object.get('to_address')  +' - '+ object.get('createdAt')  +' - '+ object.get('gas_price')  );
+    }
+  }
 
     return(
       <div>
-          <button >Show Transaction list here</button>
+          <button onClick={getAll}>Show Transaction list </button>
       </div>
     )
 }
